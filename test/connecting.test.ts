88f8585a818ba5b8d944610149fad6a3d778575e
James Ransome <jamesransome@msn.com>
2023-04-04 18:06:30 +0100

    🧪 increase number of browsers in test

--- test/connecting.test.ts
+++ test/connecting.test.ts
@@ -1,11 +1,22 @@
 import puppeteer, { Browser } from 'puppeteer';
 
-const localhost = 'http://localhost:5173';
+const url = 'http://localhost:5173';
+// const url = 'https://jransome.github.io/sudoball/';
+
 const browsers = [
   // these position args don't really work properly but it works for now
   { size: [1200, 800], position: [0, 0, 0] }, // for some reason a 3rd number forces it to open on the second monitor, but only when you're on that screen
-  { size: [900, 700], position: [1920, 800] },
-  { size: [900, 700], position: [0, 0] },
+  { size: [400, 300], position: [0, 0] },
+  { size: [400, 300], position: [0, 0] },
+  { size: [400, 300], position: [0, 0] },
+  { size: [400, 300], position: [0, 0] },
+  { size: [400, 300], position: [0, 0] },
+  { size: [400, 300], position: [0, 0] },
+  { size: [400, 300], position: [0, 0] },
+  { size: [400, 300], position: [0, 0] },
+  { size: [400, 300], position: [0, 0] },
+  { size: [400, 300], position: [0, 0] },
+  { size: [400, 300], position: [0, 0] },
 ];
 
 const waitForMs = (ms: number) => new Promise(res => setTimeout(res, ms));
@@ -29,37 +40,32 @@ describe('Connecting multiple players', () => {
       })),
     );
 
-    const [player1, player2, player3] = await Promise.all(browserInstances.map(async (b) => {
+    const [host, ...clients] = await Promise.all(browserInstances.map(async (b) => {
       const page = await b.newPage();
-      await page.goto(localhost);
+      await page.goto(url);
       return page;
     }));
 
-    await player1.click('#create-game');
-    const hostId = await player1.$eval('#host-id', input => input.getAttribute('value')) as string;
+    await host.click('#create-game');
+    const hostId = await host.$eval('#host-id', input => input.getAttribute('value')) as string;
     await waitForMs(2000);
 
-    await player2.focus('#host-id');
-    await player2.keyboard.type(hostId);
-    await player2.click('#join-game');
-
-    await player3.focus('#host-id');
-    await player3.keyboard.type(hostId);
-    await player3.click('#join-game');
+    await Promise.all(clients.map(async (client) => {
+      await client.focus('#host-id');
+      await client.keyboard.type(hostId);
+      await client.click('#join-game');
+    }));
 
     await waitForMs(15000);
 
-    await player1.keyboard.down('ArrowRight');
-    await waitForMs(1000);
-    await player1.keyboard.up('ArrowRight');
-
-    await player2.keyboard.down('ArrowRight');
-    await waitForMs(1000);
-    await player2.keyboard.up('ArrowRight');
-
-    await player3.keyboard.down('ArrowRight');
-    await waitForMs(1000);
-    await player3.keyboard.up('ArrowRight');
+    await [host, ...clients].reduce((acc, player) => {
+      acc = acc.then(async () => {
+        await player.keyboard.down('ArrowRight');
+        await waitForMs(1000);
+        await player.keyboard.up('ArrowRight');
+      });
+      return acc;
+    }, Promise.resolve());
     
     await waitForMs(10000);
   });
