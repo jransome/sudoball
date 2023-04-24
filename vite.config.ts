0720267782103333dff4c1b94e601f8c9551a206
James Ransome <jamesransome@msn.com>
2023-04-24 19:09:33 +0100

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import wasm from 'vite-plugin-wasm';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     wasm(),
   ],
   base: '/sudoball',
+  build: {
+    target: 'esnext',
+  },
 });
