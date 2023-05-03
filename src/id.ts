8064df9d6a5f62e08bad4a00cd7687f7fa2804a4
James Ransome <jamesransome@msn.com>
2023-05-03 19:16:02 +0100

    reduce size of id

--- src/id.ts
+++ src/id.ts
@@ -1,9 +1,7 @@
 import { PeerId } from './types';
 
-const animals = ['Dog', 'Donkey', 'Koala', 'Penguin', 'Bear', 'Zebra', 'Wolf', 'Gazelle', 'Hedgehog', 'Raccoon', 'Giraffe', 'Puma', 'Gorilla', 'Otter', 'Squirrel', 'Cat', 'Hyena', 'Panda', 'Monkey', 'Elephant', 'Rabbit', 'Fox', 'Kangaroo', 'Llama', 'Turtle', 'Sloth'];
-const adjectives = ['Loyal', 'Crazy', 'Debonair', 'Vivacious', 'Dulcet', 'Loving', 'Brave', 'Thicc', 'Silly', 'Grumpy', 'Cranky', 'Noble', 'Smart', 'Polite', 'Regal', 'Funny', 'Jocund', 'Lolz', 'Insatiable', 'Clingy', 'Loquacious', 'Annoying', 'Quaint', 'Zestful', 'Drunk', 'Plastered', 'Magnificent', 'Flirtatious', 'Resplendent', 'Trollied', 'Witty', 'Wise', 'Presidential', 'Cunning', 'Shy', 'Enchanting', 'Stately', 'Curious', 'Strong', 'Tall', 'Graceful', 'Chatterbox', 'Sprightly', 'Playful', 'Melodic', 'Booming', 'Fat', 'Dapper', 'Gallant', 'Nimble', 'Wistful', 'Venerable'];
+const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 
-const getRandomInt = (max: number) => Math.floor(Math.random() * max);
-const getRandomEntry = (arr: string[]) => arr[getRandomInt(arr.length)];
-
-export const generateReadableId = (): PeerId => getRandomEntry(adjectives) + getRandomEntry(animals) + getRandomInt(1000);
+export const generateId = (): PeerId => Array
+  .from({ length: 5 }, () => chars[Math.floor(Math.random() * chars.length)])
+  .join('');
